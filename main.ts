fe091204a3336a3dc21421f055cd8e267a739bb7
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-10-29 09:33:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,119 @@
+// Say message ROGER FEDERER HAS WON HIS 10 WIMBLEDON TITLE
+// Show tennis racket
+basic.showLeds(`
+    # # # . .
+    # . # . .
+    # # # . .
+    . . . # .
+    . . . . #
+    `)
+// T
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    # # . . .
+    . . # . .
+    . . . # .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . # . . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showString("ROGER FEDERER HAS WON HIS 10 WIMBLEDON TITLE")
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . # #
+    . . . # .
+    . . . # #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # # #
+    . . # . #
+    . . # # #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . #
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . # . .
+    # # # . .
+    . . . # .
+    . . . . #
+    `)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    # # . . .
+    . . # . .
+    . . . # .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . # . . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
