b0081722a5ac03b9ff101d250eca4f967f574917
JA1934 <92798990+JA1934@users.noreply.github.com>
2021-11-01 09:18:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,119 +1,135 @@
 // Say message ROGER FEDERER HAS WON HIS 10 WIMBLEDON TITLE
-// Show tennis racket
-basic.showLeds(`
-    # # # . .
-    # . # . .
-    # # # . .
-    . . . # .
-    . . . . #
-    `)
-// T
-basic.showLeds(`
-    # # . . .
-    . # . . .
-    # # . . .
-    . . # . .
-    . . . # .
-    `)
-basic.showLeds(`
-    # . . . .
-    # . . . .
-    # . . . .
-    . # . . .
-    . . # . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . .
-    . # . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showString("ROGER FEDERER HAS WON HIS 10 WIMBLEDON TITLE")
-basic.showLeds(`
-    . . . . #
-    . . . . #
-    . . . . #
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . # #
-    . . . # .
-    . . . # #
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . # # #
-    . . # . #
-    . . # # #
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . # # # .
-    . # . # .
-    . # # # .
-    . . . . #
-    . . . . .
-    `)
-basic.showLeds(`
-    # # # . .
-    # . # . .
-    # # # . .
-    . . . # .
-    . . . . #
-    `)
-basic.showLeds(`
-    # # . . .
-    . # . . .
-    # # . . .
-    . . # . .
-    . . . # .
-    `)
-basic.showLeds(`
-    # . . . .
-    # . . . .
-    # . . . .
-    . # . . .
-    . . # . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . .
-    . # . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+input.onButtonPressed(Button.A, function () {
+    // Show tennis racket
+    basic.showLeds(`
+        # # # . .
+        # . # . .
+        # # # . .
+        . . . # .
+        . . . . #
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        # # . . .
+        . . # . .
+        . . . # .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        . # . . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
+    // Tennis racket off the screen.
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    // Say ROGER FEDERER HAS WON HIS 10 WIMBLEDON TITLE
+    basic.showString("ROGER FEDERER HAS WON HIS 10 WIMBLEDON TITLE")
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . . # #
+        . . . # .
+        . . . # #
+        . . . . .
+        . . . . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . # # #
+        . . # . #
+        . . # # #
+        . . . . .
+        . . . . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . # . .
+        # # # . .
+        . . . # .
+        . . . . #
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        # # . . .
+        . . # . .
+        . . . # .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        . # . . .
+        `)
+    // Tennis racket Across the screen
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
+    // Tennis Racket off the screen. End code.
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
 basic.forever(function () {
 	
 })
